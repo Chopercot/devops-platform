7450798a8173131577ff520d81761ca071b7935d
Chopercot <cottt@inbox.ru>
2026-03-26 22:26:03 +0300

    feat: add http_requests_total metric and integrate with Prometheus/Grafana

--- go-service/cmd/server/main.go
+++ go-service/cmd/server/main.go
@@ -3,30 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var httpRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total number of HTTP requests",
+	},
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	httpRequests.Inc()
+	fmt.Fprintf(w, "DevOps Platform is running 🚀\n")
+}
+
 func main() {
-	version := os.Getenv("APP_VERSION")
-	if version == "" {
-		version = "unknown"
-	}
+	prometheus.MustRegister(httpRequests)
 
-	// 🔥 отдельный handler для /
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		fmt.Fprintf(w, "DevOps Platform is running 🚀 (%s)\n", version)
-	})
-
-	// ✅ метрики отдельно
+	http.HandleFunc("/", handler)
 	http.Handle("/metrics", promhttp.Handler())
 
 	fmt.Println("Server started on :8080")
-
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
